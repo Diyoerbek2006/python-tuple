fd7b679ae2920651e03a99a1046fb9c3ff00850c
djumanov <djumanovdev@gmail.com>
2025-12-27 19:48:17 +0500

    done

---
+++ task01.py
@@ -0,0 +1,15 @@
+people = [("Ali", 24), ("Laylo", 30), ("Jasur", 39)]
+
+
+def find_oldest_person(people: list[tuple[str, int]]) -> tuple[str, int] | None:
+    if len(people) == 0:
+        return None
+    
+    oldest_person = people[0]
+    for person in people:
+        if person[1] > oldest_person[1]:
+            oldest_person = person
+
+    return oldest_person
+
+print(find_oldest_person(people))
---
+++ task02.py
@@ -0,0 +1,10 @@
+
+nums = [2, 3, 5, 3, 7, 9, 2, 4, 5, 3, 7, 9, 0]
+
+unique_nums = set(nums)
+# unique_nums = []
+# for num in nums:
+#     if num not in unique_nums:
+#         unique_nums.append(num)
+
+print(unique_nums)
